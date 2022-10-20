ce6f25c520acfe1dbf74126f62d5c270e21108d1
ali tunacan onar <alitunacanonar@icloud.com>
2022-10-20 11:33:19 +0300

    Web server index file editing

--- webServer/index.js
+++ webServer/index.js
@@ -8,22 +8,22 @@ const server = http.createServer((req, res) => {
 
     if (url === "/") {
         res.writeHead(200, { "Content-Type": "text/html" });
-        res.write("<h1>INDEX SAYFASI</h1>");
+        res.write("<h2>Welcome to the Index Page</h2>");
     } else if (url === "/about") {
         res.writeHead(200, { "Content-Type": "text/html" });
-        res.write("<h1>ABOUT SAYFASI</h1>");
+        res.write("<h2>Welcome to the About Page</h2>");
     } else if (url === "/contact") {
         res.writeHead(200, { "Content-Type": "text/html" });
-        res.write("<h1>CONTACT SAYFASI</h1>");
+        res.write("<h2>Welcome to the Contact Page</h2>");
     } else {
         res.writeHead(404, { "Content-Type": "text/html" });
-        res.write("<h1>404 SAYFA BULUNAMADI</h1>");
+        res.write("<h2>404 SAYFA BULUNAMADI</h2>");
     }
 
     res.end();
 });
 
-const port = 3000;
+const port = 5000;
 
 server.listen(port, () => {
     console.log(`Sunucu port ${port} de başlatıldı.`);
